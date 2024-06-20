ed85893988b1d035503a67de59803778e5ee563a
gondaliyarutam <rutamgondaliya666@gmail.com>
2024-06-20 12:18:37 +0530

    Add files via upload

---
+++ task 1_2_1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+
+ {
+ 	
+ 	printf("name :  rutam \n ");
+ 	printf("surename : gondaliya  \n");
+ 	printf("father name = bhikhu bhai \n");
+ 	printf("age : 18\n");
+ 	printf("school : vivekanand  vidhyalay");
+ }
